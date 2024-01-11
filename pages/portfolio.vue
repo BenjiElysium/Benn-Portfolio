b10bc1ea03804acd9da43e35fbafb232cd5f9b81
Philip <philipaldenbenn@gmail.com>
2024-01-10 17:07:06 -0800

    Initial commit styling

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -9,14 +9,15 @@ const mediaAssets = [
 <style>
 .gallery-wrapper {
   max-width: 80%; /* Adjust as needed */
-  margin-left: 30px;
+  margin-left: 20px;
   margin-right: auto;
 }
 </style>
 
+
 <template>
-  <div class="pt-12 mb-20">
-      <h1 class="text-3xl font-bold ms-14 m-8">Portfolio: Product and Environment Renders</h1>
+  <div class="pt-20 mt-4 mb-20">
+      <h1 class="text-3xl font-bold ms-6 mb-2">Portfolio: Product and Environment Renders</h1>
       <div class="pt-2">
         <div class="gallery-wrapper">
           <CldProductGallery :media-assets="mediaAssets" />
