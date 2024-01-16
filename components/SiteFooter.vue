994e18a09b026fa0c8028ae26340b58060e8fa0b
Philip <philipaldenbenn@gmail.com>
2024-01-16 07:54:03 -0800

    ready to publish 4

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -18,9 +18,9 @@ const links = [
 
 <template>
   <footer class="footer bg-zinc-900/90">
-    <section class="max-w-2xl px-4 mt-2 m-4 justify-between text-gray-300 flex flex-row">
-      <p class="mt-0">&copy; Philip Benn 2023. All Rights Reserved</p>
-      <div class="flex flex-row gap-4">
+    <section class="max-w-2xl px-1 md:px-4 mt-2 m-2 md:m-4 justify-between text-gray-300 flex flex-row">
+      <p class="text-xs md:text-sm mt-2 md:mt-0">&copy; Philip Benn 2023. All Rights Reserved</p>
+      <div class="flex flex-row m-1 md:m-[-1%] gap-2 md:gap-4">
         <a v-for="link in links" :key="link.name" :href="link.href">
           <Icon :name="link.icon" size="1.5rem" class="text-gray-300" />
         </a>
